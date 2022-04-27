82229ffb125bf493068f983e3421cc28d8984763
TaniDash <99218504+TaniDash@users.noreply.github.com>
2022-04-26 23:27:52 -0700

    committed

--- src/test/java/test/SingleItemRemoveTest.java
+++ src/test/java/test/SingleItemRemoveTest.java
@@ -1,18 +1,16 @@
 package test;
 
 import java.util.List;
-import java.util.Random;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
-
 import pages.BasePage;
 import pages.CheckboxValidationPage;
 import util.BrowserFactory;
 
-public class SingleItemRemoveTest extends BasePage {
+public class SingleItemRemoveTest extends BasePage{
 
 	WebDriver driver;
 	CheckboxValidationPage checkboxValidation;
@@ -23,13 +21,15 @@ public class SingleItemRemoveTest extends BasePage {
 		driver= BrowserFactory.init();
 		checkboxValidation= PageFactory.initElements(driver, CheckboxValidationPage.class);
 		checkboxValidation.addSingleItem(insertName);
+		checkboxValidation.addSingleItem(insertName);
 		
+//		String beforCssPath = "input[name='todo[";
+//		String afterCssPath = "]']";
 		
-
+		List<WebElement>myList2= driver.findElements(By.xpath("//div[@id='todos-content']/child::*/ul//li//descendant::input"));
+		System.out.println(myList2.size());
 		
-	}
-
-	
-	
+		checkboxValidation.validateSingleListItemRemoved(myList2.size(), myList2);
 		
+}
 }
